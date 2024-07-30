ad88ca8fbdfeace8bdc19dac19810eaa21c56a28
Camellia.xiaohua <camelliaxiaohua@gmail.com>
2024-07-30 23:01:49 +0800

    Update EnumValue.java

--- mybatis-plus-annotation/src/main/java/com/baomidou/mybatisplus/annotation/EnumValue.java
+++ mybatis-plus-annotation/src/main/java/com/baomidou/mybatisplus/annotation/EnumValue.java
@@ -1,18 +1,15 @@
 /*
- * Copyright (c) 2011-2024, baomidou (jobob@qq.com).
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+*版权所有（c）2011-2024，baomidou（jobob@qq.com）。
+*根据Apache许可证，版本2.0（“许可证”）许可；
+*除非符合许可证，否则您不得使用此文件。
+*您可以在以下处获得许可证的副本
+* Http://www.apache.org/licenses/LICENSE-2.0
+*除非适用法律要求或书面同意，否则软件
+*在“按原样”的基础上分发，
+*没有任何明示或暗示的保证或条件。
+*有关管理权限的特定语言，请参阅许可证和
+*许可证下的限制。
+*/
 package com.baomidou.mybatisplus.annotation;
 
 import java.lang.annotation.*;
